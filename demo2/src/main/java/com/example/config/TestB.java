659f2d9aff2de740d1f31df4ebec608d60d2a8f9
tlh <vlingl@163.com>
2021-03-05 12:57:00 +0800

    update: 更新b4分支

---
+++ demo2/src/main/java/com/example/config/TestB.java
@@ -0,0 +1,11 @@
+package com.example.config;
+
+/**
+ * @author TANG
+ * @date 2021-03-05
+ */
+public class TestB {
+    public static void main(String[] args) {
+        System.out.println("啥是啥 = ");
+    }
+}
