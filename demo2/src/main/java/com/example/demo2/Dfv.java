3ec902d4100a10006e23aa2649a889afefd48508
tlh <vlingl@163.com>
2021-03-05 12:38:14 +0800

    update: 更新b4分支

--- demo2/src/main/java/com/example/demo2/Dfv.java
+++ demo2/src/main/java/com/example/demo2/Dfv.java
@@ -8,4 +8,12 @@ public class Dfv {
     public static void main(String[] args) {
         System.out.println("22222 = ");
     }
+
+    public static void m1(String[] args) {
+        System.out.println("m1 = ");
+    }
+
+    public static void m2(String[] args) {
+        System.out.println("m2 = ");
+    }
 }
