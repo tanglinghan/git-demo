44b35d5dc8fe9a2e5233e126818a3a3b8247566e
tlh <vlingl@163.com>
2021-03-05 10:19:14 +0800

    add: 新的提交

---
+++ demo/src/main/java/com/example/demo/TestClass.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+/**
+ * 测试类
+ *
+ * @author TANG
+ * @date 2021-03-05
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println(" 这是一句话....");
+        System.out.println(" 这是一句话....");
+        System.out.println(" 这是一句话....");
+        System.out.println(" 这是一句话....");
+        System.out.println(" 这是一句话....");
+        System.out.println(" 这是一句话....");
+    }
+}
