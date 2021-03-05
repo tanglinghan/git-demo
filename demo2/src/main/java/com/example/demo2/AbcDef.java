bc4894136b91db28e5baa0988973f206c324296f
tlh <vlingl@163.com>
2021-03-05 12:22:50 +0800

    update: 更新b4分支

--- demo2/src/main/java/com/example/demo2/AbcDef.java
+++ demo2/src/main/java/com/example/demo2/AbcDef.java
@@ -8,4 +8,9 @@ public class AbcDef {
     public static void main(String[] args) {
         System.out.println("111111 = ");
     }
+
+    public void method1(){
+        System.out.println("1 = " + 1);
+    }
+
 }
---
+++ demo2/src/main/java/com/example/demo2/Dfv.java
@@ -0,0 +1,11 @@
+package com.example.demo2;
+
+/**
+ * @author TANG
+ * @date 2021-03-05
+ */
+public class Dfv {
+    public static void main(String[] args) {
+        System.out.println("22222 = ");
+    }
+}
