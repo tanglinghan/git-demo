bccd2c8c1fb310ea053632867ef297a202f74819
tlh <vlingl@163.com>
2021-03-05 10:40:21 +0800

    add: 主分支添加TestClass2

---
+++ demo/src/main/java/com/example/demo/TestClass2.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+/**
+ * 测试类
+ *
+ * @author TANG
+ * @date 2021-03-05
+ */
+public class TestClass2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("按地区奥多群多群奥多无群奥翁奥多");
+
+    }
+}
